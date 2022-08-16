e24da08c29f60e168e6ae242605b727313acb018
jorgebritos <83886610+jorgebritos@users.noreply.github.com>
2022-08-16 17:20:42 -0300

    login integration

---
+++ public/js/app.js
@@ -0,0 +1,64 @@
+let auth0 = null;
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+
+window.onload = async () => {
+  await configureClient();
+
+  // .. code ommited for brevity
+
+  updateUI();
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    // show the gated content
+    return;
+  }
+
+  // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+
+    // Process the login state
+    await auth0.handleRedirectCallback();
+
+    updateUI();
+
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+};
+
+const updateUI = async () => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+
+  // NEW - add logic to show/hide gated content after authentication
+  if (isAuthenticated) {
+    window.location.replace("http://localhost:3000/index.html");
+  }
+};
+
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+  });
+};
+
+const logout = () => {
+  auth0.logout({
+    returnTo: window.location.origin
+  });
+};
