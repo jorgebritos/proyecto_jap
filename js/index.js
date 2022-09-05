e92c0776d894d5e5cd3a354c99e168ee1d3c8896
jorgebritos <83886610+jorgebritos@users.noreply.github.com>
2022-09-04 22:32:42 -0300

    hotfix

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 function redirect() {
+    let correo = document.getElementById("emailInput");
+    let pass = document.getElementById("passInput");
     localStorage.setItem("email", document.getElementById('emailInput').value)
+    console.log(location.href)
+    if (!correo.value || !pass.value) return alert("Debe completar los campos");
+    location.href="./main.html";
 }
