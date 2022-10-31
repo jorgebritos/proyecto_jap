e4203f49af2535aa4cd565bd3665e7d8aee0aa9c
jorgebritos <83886610+jorgebritos@users.noreply.github.com>
2022-10-30 23:12:15 -0300

    entrega 6

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
 let subtotalGeneral = 0;
-let iva = 0;
+let costoEnvio = 0;
 let tipoPago = "";
 let activated = [];
 
@@ -10,7 +10,6 @@ let descPaymentType = document.getElementById("descPaymentType");
 let errPaymentType = document.getElementById("err-pt")
 let inputsTarjeta = document.getElementsByClassName("input-tarjeta")
 let inputsTransferencia = document.getElementsByClassName("input-transferencia")
-
 
 //AQUI SE CALCULA EL SUBTOTAL DEL APARTADO DE COSTOS, ADEMÁS DE TRANSFORMAR LOS PRECIOS QUE SEAN DE PESOS A DÓLARES
 let calcularSubtotal = function () {
@@ -38,7 +37,7 @@ let calcularIva = function () {
     // VER CUAL CLASE DE ENVIO EL USUARIO TIENE SELECCIONADO
     for (const r of claseEnvios) {
         if (r.checked) {
-            iva = subtotalGeneral * Number(r.value);
+            costoEnvio = subtotalGeneral * Number(r.value);
         }
     }
 
@@ -48,12 +47,12 @@ let calcularIva = function () {
 
 let mostrarTotal = function () {
 
-    totalPrecio = Number(subtotalGeneral) + Number(iva);
+    totalPrecio = Number(subtotalGeneral) + Number(costoEnvio);
 
     // AQUÍ HARDCODEO 'USD' DEBIDO A QUE NO ME PARECIÓ NECESARIO TRAREME LA CURRENCY DE CUALQUIER ARTÍCULO, YA QUE
     // DE UNA FORMA U OTRA SIEMPRE DEBE SER DÓLARES
     subtotal.innerHTML = "USD " + subtotalGeneral;
-    tipoEnvio.innerHTML = "USD " + iva.toFixed(2);
+    tipoEnvio.innerHTML = "USD " + costoEnvio.toFixed(2);
     total.innerHTML = "USD " + totalPrecio.toFixed(2);
 
 }
@@ -113,79 +112,6 @@ let mostrarTipoPago = function () {
 
 }
 
-// 'use strict'
-
-// AQUÍ ES CUANDO SE ACTIVAN LOS FEEDBACK DE LOS INPUT DEL FORMULARIO CUANDO EL USUARIO NO COMPLETA LOS DATOS
-// O SE BORRAN LOS DATOS Y SE MUESTRA EL POP-UP SI SE HACE CORRECTAMENTE
-
-var form = document.querySelector('.needs-validation')
-
-form.addEventListener('submit', function (event) {
-
-    for (const i of activated) {
-        //EN CASO DE QUE DENTRO DEL MODAL, ALGUNO DE LOS CAMPOS SELECCIONADOS ESTÉN VACÍOS, EN VEZ DE MOSTRAR UN ERROR
-        //DEBAJO DEL TIPO DE PAGO EN LA PÁGINA PRINCIPAL, SE DESPLIEGA EL MISMO MODAL PARA QUE EL USUARIO VEA EL ERROR
-        if (i.value.length < 1) document.getElementById("btn-modal").click();
-    }
-    if (tipoPago === "") {
-        //SI EL USUARIO NO SELECCIONÓ NINGUN MÉTODO DE PAGO...
-        errPaymentType.innerHTML = "Debe seleccionar una forma de pago"
-    } else {
-        errPaymentType.innerHTML = ""
-    }
-    if (!form.checkValidity()) {
-        form.classList.add('was-validated')
-        event.preventDefault()
-        event.stopPropagation()
-    } else {
-        let success = document.getElementById("success");
-
-        //EN VEZ DE CREAR EL POP-UP EN EL HTML Y DARLE LA CLASE DE SHOW DESDE JS, DECIDÍ HACERLO DE ESTA FORMA, DEBIDO A QUE
-        //EN EL GRUPAL 1, TENÍAMOS ALGO PARECÍDO, Y EN CASO DE QUE APRETARAMOS EL BOTÓN 2 VECES, TIRABA ERROR Y NO LO MOSTRABA.
-        //DE ESTA FORMA SE EVÍTA DICHO ERROR.
-        success.innerHTML = `
-            <div class="alert alert-success alert-dismissible fade" role="alert" id="alert-success">
-                <p>La compra se ha realizado correctamente!</p>
-                <button type="button" class="btn-close" id="cerrarPopup" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>`
-        document.getElementById("alert-success").classList.add("show");
-
-        //PARA QUE CUANDO SE QUITEN LOS DATOS DE LOS INPUTS, NO SIGA MOSTRANDO LOS FEEDBACK
-        form.classList.remove('was-validated')
-
-        //LUEGO DE 4 SEGUNDOS, SE ELIMINA AUTOMÁTICAMENTE EL POP-UP
-        setTimeout(() => {
-            document.getElementById("cerrarPopup").click();
-        }, 4000);
-
-        //ELIMINANDO DATOS LUEGO DE COMPLETAR CORRECTAMENTE EL FORMULARIO (NECESITA REFACTORING)
-
-        for (const e of controls) {
-            console.log(e.innerHTML)
-            e.value = "";
-        }
-
-        for (const e of inputsTarjeta) {
-            e.value = "";
-        }
-
-        for (const e of inputsTransferencia) {
-            e.value = "";
-        }
-
-        for (const r of clasePagos) {
-            r.checked = false;
-        }
-
-        //PARA QUE ENTRE AL DEFAULT DEL SWITCH
-        tipoPago = "";
-
-        // PARA QUE SE REINICIE EL TEXTO QUE MUESTRA EL TIPO DE PAGO
-        mostrarTipoPago();
-    }
-
-}, false)
-
 // (EVENTLISTENERS AL FINAL DEL DOCUMENTO)
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -224,7 +150,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 let newCell = newItem.insertCell();
                 let input = document.createElement("INPUT");
 
-
                 //EN TODAS LAS CASILLAS QUE UTILIZO DIRECTAMENTE, LES COLOCO EL NOMBRE DEL MISMO PRODUCTO, MÁS EL TIPO DE CASILLA
                 //COMO SU ID. ESTO PARA QUE SEA MAS FÁCIL HACERLE SABER AL DOCUMENTO QUE CASILLAS ESPECÍFICAS QUIERO UTILIZAR
                 //(SIENTO QUE SERÍA MUCHO MÁS LIMPIO UTILIZAR EL ID EN VEZ DEL NOMBRE, PERO LO HARÉ CUANDO VUELVA A REFACTORIZAR)
@@ -249,13 +174,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
                         document.getElementById(`${itteratorProduct['name']}Count`).addEventListener("input", function () {
 
-
                             let subtotal = document.getElementById(`${itteratorProduct['name']}Subtotal`).childNodes[0];
                             let costoUnitario = document.getElementById(`${itteratorProduct['name']}UnitCost`).childNodes[0].innerHTML.split(" ").splice(1, 1)[0];
                             let cantidad = document.getElementById(`${itteratorProduct['name']}Count`).value;
                             subtotal.innerHTML = costoUnitario * cantidad;
-
-
 
                             calcularSubtotal(cantidad);
                         })
@@ -288,7 +210,6 @@ document.addEventListener("DOMContentLoaded", function () {
             p.appendChild(newText)
             newCell.appendChild(p);
 
-
             // CASILLA DE ELIMINAR PRODUCTO
             let deleteButton = newItem.insertCell();
             let deleteIcon = document.createElement('button');
@@ -311,6 +232,78 @@ document.addEventListener("DOMContentLoaded", function () {
         calcularIva();
         mostrarTipoPago();
     }
+
+    // AQUÍ ES CUANDO SE ACTIVAN LOS FEEDBACK DE LOS INPUT DEL FORMULARIO CUANDO EL USUARIO NO COMPLETA LOS DATOS
+    // O SE BORRAN LOS DATOS Y SE MUESTRA EL POP-UP SI SE HACE CORRECTAMENTE
+
+    var form = document.querySelector('.needs-validation')
+
+    form.addEventListener('submit', function (event) {
+
+        for (const i of activated) {
+            //EN CASO DE QUE DENTRO DEL MODAL, ALGUNO DE LOS CAMPOS SELECCIONADOS ESTÉN VACÍOS, EN VEZ DE MOSTRAR UN ERROR
+            //DEBAJO DEL TIPO DE PAGO EN LA PÁGINA PRINCIPAL, SE DESPLIEGA EL MISMO MODAL PARA QUE EL USUARIO VEA EL ERROR
+            if (i.value.length < 1) document.getElementById("btn-modal").click();
+        }
+        if (tipoPago === "") {
+            //SI EL USUARIO NO SELECCIONÓ NINGUN MÉTODO DE PAGO...
+            errPaymentType.innerHTML = "Debe seleccionar una forma de pago"
+            form.classList.add('was-validated')
+            return 1;
+        } else {
+            errPaymentType.innerHTML = ""
+        }
+        if (!form.checkValidity()) {
+            form.classList.add('was-validated')
+            event.preventDefault()
+            event.stopPropagation()
+        } else {
+            let success = document.getElementById("success");
+
+            //EN VEZ DE CREAR EL POP-UP EN EL HTML Y DARLE LA CLASE DE SHOW DESDE JS, DECIDÍ HACERLO DE ESTA FORMA, DEBIDO A QUE
+            //EN EL GRUPAL 1, TENÍAMOS ALGO PARECÍDO, Y EN CASO DE QUE APRETARAMOS EL BOTÓN 2 VECES, TIRABA ERROR Y NO LO MOSTRABA.
+            //DE ESTA FORMA SE EVÍTA DICHO ERROR.
+            success.innerHTML = `
+            <div class="alert alert-success alert-dismissible fade" role="alert" id="alert-success">
+                <p>La compra se ha realizado correctamente!</p>
+                <button type="button" class="btn-close" id="cerrarPopup" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>`
+            document.getElementById("alert-success").classList.add("show");
+
+            //PARA QUE CUANDO SE QUITEN LOS DATOS DE LOS INPUTS, NO SIGA MOSTRANDO LOS FEEDBACK
+            form.classList.remove('was-validated')
+
+            //LUEGO DE 4 SEGUNDOS, SE ELIMINA AUTOMÁTICAMENTE EL POP-UP
+            setTimeout(() => {
+                document.getElementById("cerrarPopup").click();
+            }, 4000);
+
+            //ELIMINANDO DATOS LUEGO DE COMPLETAR CORRECTAMENTE EL FORMULARIO (NECESITA REFACTORING)
+
+            for (const e of controls) {
+                e.value = "";
+            }
+
+            for (const e of inputsTarjeta) {
+                e.value = "";
+            }
+
+            for (const e of inputsTransferencia) {
+                e.value = "";
+            }
+
+            for (const r of clasePagos) {
+                r.checked = false;
+            }
+
+            //PARA QUE ENTRE AL DEFAULT DEL SWITCH
+            tipoPago = "";
+
+            // PARA QUE SE REINICIE EL TEXTO QUE MUESTRA EL TIPO DE PAGO
+            mostrarTipoPago();
+        }
+
+    }, false)
 
     claseEnvios.forEach((radio) => {
         radio.addEventListener("click", () => {
